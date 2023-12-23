da336e18d7c7c1810bb350d569ddb0a9d8bcbda2
tuantv16 <vantuant2@gmail.com>
2023-12-23 13:26:43 +0700

    m

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UsersTableSeeder::class,
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            RoleUserSeeder::class,
+            PermissionRoleSeeder::class,
+        ]);
     }
 }
